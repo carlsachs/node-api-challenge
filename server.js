27f79e23bd884d8fce290dc789203a5aa1e0305c
carlsachs <sachscarl@gmail.com>
2020-03-13 13:12:54 -0400

    MVP, added the actions router page and checked all endpoints

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const projectRouter = require('./api/projectRouter')
 
 server.use(express.json())
 server.use(helmet())
+server.use('/api/projects', projectRouter)
+server.use('/api/actions', actionRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Welcome Carlton!</h2>`)
 })
 
-server.use('/api/projects', projectRouter)
+// server.use('/api/projects', projectRouter)
+// server.use('/api/actions', actionRouter)
 
 module.exports = server;
